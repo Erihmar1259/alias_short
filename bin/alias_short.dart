64e5daa203e303ed9dda689ac924e7a57da46744
erihmar <stz.erihmar@gmail.com>
2024-09-11 17:11:46 +0800

    Initial commit

---
+++ bin/alias_short.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+void main(List<String> arguments) {
+  if (arguments.isEmpty) {
+    print('No command provided.');
+    return;
+  }
+
+  final command = arguments.first;
+
+  // Define your command aliases
+  final commandsMap = {
+    'apk': 'flutter build apk --release',
+    'ios': 'flutter build ios',
+    // Add more aliases here as needed
+  };
+
+  if (commandsMap.containsKey(command)) {
+    runCommand(commandsMap[command]!);
+  } else {
+    print('Unknown command: $command');
+  }
+}
+
+void runCommand(String command) async {
+  // Run the mapped Flutter command
+  Process.run(command.split(' ').first, command.split(' ').skip(1).toList()).then((result) {
+    stdout.write(result.stdout);
+    stderr.write(result.stderr);
+  });
+}
---
+++ test/alias_short_test.dart
@@ -0,0 +1,8 @@
+import 'package:alias_short/alias_short.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
